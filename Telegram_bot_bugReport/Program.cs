dfa8d39a58768c02ad7453f7c2a55c0934c0c8e2
patty98 <linascabeskrovnaya@gmail.com>
2020-02-22 08:15:28 +0300

    Fix Security

--- Telegram_bot_bugReport/Program.cs
+++ Telegram_bot_bugReport/Program.cs
@@ -18,9 +18,8 @@ namespace Telegram_bot_bugReport
         {
             //var proxy = new HttpToSocks5Proxy("98.143.145.29", 62354);
             var proxyObject = new WebProxy("157.245.224.29:3128");
-            BotActions Bot = new BotActions("linascabeskrovnaya@gmail.com:OTaP5ANjI3I287bGrgwK20D5", "934475293:AAHgthyLni4IMpGHaSR6eU9jx5Ocj86e5z0");
-            //    Bot.Bot = new TelegramBotClient("934475293:AAHgthyLni4IMpGHaSR6eU9jx5Ocj86e5z0",proxyObject);
-            // Bot.StartReceiving();
+            BotActions Bot = new BotActions("", "");
+            
            
             Bot.InitializationBot(proxyObject);
             Bot.Bot.StartReceiving();
